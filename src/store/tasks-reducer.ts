94e5e569be0a8ceac310c75a19a7dd8bc9989fec
Tsyriulnikov <max_djvu@mail.ru>
2022-04-25 14:07:49 +0300

    lesson_9 reducers v1

---
+++ src/store/tasks-reducer.ts
@@ -0,0 +1,9 @@
+
+export const tasksReducer = (state:any, action:any)=> {
+    switch (action.type) {
+        case "sdsdsd":{
+            return state
+        }
+        default: throw new Error("error...")
+    }
+}
---
+++ src/store/todolists-reducer.ts
@@ -0,0 +1,8 @@
+export const todoListsReducer=(state:any,action:any):any => {
+    switch (action.type) {
+        case "sdfsdf":{
+return state
+        }
+            default: throw new Error("error...")
+    }
+}
