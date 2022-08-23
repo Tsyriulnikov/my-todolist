c1d2ede648a001d2532e7180b95975c12e6fd306
Tsyriulnikov <max_djvu@mail.ru>
2022-08-23 22:06:46 +0300

    lesson 22_2

--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -120,7 +120,12 @@ export const slice = createSlice({
 
 // types
 export type UpdateDomainTaskModelType = {
-   
+    title?: string
+    description?: string
+    status?: TaskStatuses
+    priority?: TaskPriorities
+    startDate?: string
+    deadline?: string
 }
 export type TasksStateType = {
     [key: string]: Array<TaskType>
