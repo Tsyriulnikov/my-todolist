5c41ba3317255969a7f35b5b467680977c6d1eb1
Tsyriulnikov <max_djvu@mail.ru>
2022-08-26 11:24:58 +0300

    lesson 22_4

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -64,12 +64,7 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
                     let allTodolistTasks = tasks[tl.id]
 
                     return <Grid item key={tl.id}>
-                        <div style={{width: '300px'}}>
-                            <Todolist
-                                todolist={tl}
-                                tasks={allTodolistTasks}
-                                demo={demo}
-                            />
+                        
                         </div>
                     </Grid>
                 })
